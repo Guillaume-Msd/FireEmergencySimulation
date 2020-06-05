e1f1a0f7be8a8952ab396e328352c228da96c8f1
thibault-gilg <thibault.gilg@yahoo.fr>
2020-06-05 16:26:34 +0400

    init environment  elements

--- FireSimulation/src/model/EnvironmentElement.java
+++ FireSimulation/src/model/EnvironmentElement.java
@@ -1,25 +1,63 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 
 public class EnvironmentElement implements ElementInterface {
 
-	private List<Integer> capacity;
+	private int maxCapacity;
 	
-	private Coord localisation;
+	private int actualCapacity;
 	
-	public EnvironmentElement(Coord localisation) {
-		//this.localisation = localisation;
-		//List <Integer> capacity = new ArrayList<Integer>();
-		
+	private Coord location;
+	
+	private TypeElement type;
+	
+	public EnvironmentElement(Coord location, int maxCapacity, TypeElement type) {
+		this.location = location;
+		this.maxCapacity = maxCapacity;
+		this.actualCapacity = this.maxCapacity;
+		this.setType(type);
 	}
     
 	@Override
-	public void supplyVehicule() {
-		// TODO Auto-generated method stub
+	public void supplyVehicule(InterventionVehicule vehicule) {
+		//TODO Set le liquide du camion à la valeur de sa capactité max
 		
 	}
+	
+	public void decreaseActualCapacity(int liquid) {
+		this.actualCapacity -= liquid;
+	}
 
+	public int getMaxCapacity() {
+		return maxCapacity;
+	}
+
+	private void setMaxCapacity(int maxCapacity) {
+		this.maxCapacity = maxCapacity;
+	}
+
+	public int getActualCapacity() {
+		return actualCapacity;
+	}
+
+	public void setActualCapacity(int actualCapacity) {
+		this.actualCapacity = actualCapacity;
+	}
+
+	public Coord getLocation() {
+		return location;
+	}
+
+	public void setLocation(Coord location) {
+		this.location = location;
+	}
+
+	public TypeElement getType() {
+		return type;
+	}
+
+	private void setType(TypeElement type) {
+		this.type = type;
+	}
+	
 }

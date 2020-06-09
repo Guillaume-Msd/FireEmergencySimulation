cde7da00aff4a397f791232051de703109cbccd5
thibault-gilg <thibault.gilg@yahoo.fr>
2020-06-09 10:19:34 +0400

    fire changes

--- FireSimulation/src/simulation/Simulator.java
+++ FireSimulation/src/simulation/Simulator.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
-import java.util.Set;
 
 import controller.InterventionController;
 import controller.EventController;
@@ -15,44 +14,41 @@ import model.Event;
 import model.Fire;
 import model.FireIntensity;
 import model.FireType;
-import model.LiquidEnum;
 import model.Vehicule;
 
 public class Simulator {
 	
-	private EventController simulationController;
+	private EventController eventController;
 	private InterventionController interventionController;
 
 	
-	/**initialise le simulateur avec les controleurs gerant les elements et les evenements
+	/**
+	 * initialise le simulateur avec les controleurs gerant les elements et les evenements
 	 * @throws IOException
 	 */
 	public Simulator() throws IOException {
-		this.simulationController = new EventController();
+		this.eventController = new EventController();
 		this.interventionController = new InterventionController();
 	}
 
 	/**
-	 * Renvoie le temps entre la creation de chaque feu en fonction d'un coefficient de sensibilite
-	 * fixe et de la difficulte choisie par l'utilisateur
-	 * @param sensisitivity s
-	 * @param difficulty d
+	 * Renvoie le temps aleatoire entre la creation de chaque feu en fonction d'une valeur moyenne
+	 * @param mean 
 	 * @return time
 	 */
-	public long timelapse(int s, int d) {
+	public int timelapse(int mean) {
 		Random r = new Random();
-		long time = (long) (1000*s/d + 0.5*(r.nextInt(1000*s*(int) ((2/d))) - 1000*s/d));
+		int time = mean*1000 + (r.nextInt(2*mean*1000) - 1000*mean)/3;
 		return time;
 	}
 	
 	
-	/**crée un Feu d'une intensité aléatoire et à des coords aléatoires
+	/**cree un Feu d'un type aleatoire et a des coords aleatoires
 	 * @param mapSize
 	 * @throws IOException
 	 */
 	public void newFire(int mapSize) throws IOException {
 		Random r = new Random();
-		
 		int x = r.nextInt(mapSize);
 		int y = r.nextInt(mapSize);
 		
@@ -64,11 +60,18 @@ public class Simulator {
 		int i = r.nextInt(FireType.listTypes.size());
 		FireType type = FireType.listTypes.get(i);
 		Fire fire = new Fire(new Coord(x, y), type, FireIntensity.Low);
-		this.simulationController.createEvent(fire);
+		this.eventController.createEvent(fire);
 	}
 	
+	/**
+	 * verifie que les feux crees sont suffisamment espaces
+	 * @param x
+	 * @param y
+	 * @return
+	 * @throws IOException
+	 */
 	private boolean isFireLocation(int x, int y) throws IOException {
-		List<Coord> coordList = simulationController.getAllEventCoords();
+		List<Coord> coordList = eventController.getAllEventCoords();
 		for(Coord coord: coordList) {
 			if(Math.abs(coord.x - x) < 5 && Math.abs(coord.y - y) < 5) {
 				return true;
@@ -82,11 +85,11 @@ public class Simulator {
 	 * @throws IOException
 	 */
 	public void aggravateFire() throws IOException {
-		Event[] listEvent = this.simulationController.getAllEvents();
+		Event[] listEvent = this.eventController.getAllEvents();
 		for (Event f : listEvent) {
 			if (f instanceof Fire) {
 				if (SAggrave(((Fire) f).getIntensity())) {
-					simulationController.updateEvent(f, ((Fire) f).aggravate(), "aggraver");
+					eventController.updateEvent(f, ((Fire) f).aggravate(), "aggraver");
 				}
 			}
 		}
@@ -100,7 +103,7 @@ public class Simulator {
 	 * @throws IOException
 	 */
 	public void manageIntervention() throws IOException {
-		Event[] events = this.simulationController.getAllEvents();
+		Event[] events = this.eventController.getAllEvents();
 		List<Event> listEvent = new ArrayList<Event>();
 		int i;
 		for(i = 0; i < events.length; i++) {
@@ -116,21 +119,20 @@ public class Simulator {
 					if (coordEvent.isInRange(vehicule.getCoord(), vehicule.getRange())) {
 						vehiculeAEteint = true;
 						if (estEfficace(((Fire) event).getIntensity())) {
-							this.simulationController.updateEvent(event, ((Fire) event).attenuate(), "attenuer");
+							this.eventController.updateEvent(event, ((Fire) event).attenuate(), "attenuer");
 						}
-						System.err.println(vehicule.getQuantiteEau());
-						vehicule.decreaseLiquid();
+						System.err.println(vehicule.getWaterQuantity());
+						vehicule.decreaseWater();
+						this.interventionController.updateVehiculeWater(vehicule);
 						if(this.checkLiquidQuantity(vehicule)) {
 							this.sendRavitaillement(vehicule);
 						}
 						else{
 							if((event.getLocalisation().size() <= 1)) {
-								this.simulationController.deleteEvent(event);
+								this.eventController.deleteEvent(event);
 								vehicule.setStatut(EnumStatut.FinDIntervention);
 								this.interventionController.updateVehiculeStatut(vehicule);
 							}
-							
-
 						}
 					}				
 				}
@@ -149,31 +151,42 @@ public class Simulator {
 	 * @throws IOException
 	 */
 	public boolean checkLiquidQuantity(Vehicule vehicule) throws IOException {
-		if(vehicule.getQuantiteEau() < vehicule.getLiquidDecrease()) {
+		if(vehicule.getWaterQuantity() < vehicule.getWaterDecrease()) {
 			return true;
 		} 
 		return false;
 		
 	}
 	
+	/**
+	 * envoie le vehicule a une bouche d'incendie en changeant son statut
+	 * @param vehicule
+	 * @throws IOException
+	 */
 	public void sendRavitaillement(Vehicule vehicule) throws IOException {
 		System.err.println("RAVITAILLEMENT");
 		vehicule.setStatut(EnumStatut.BesoinRavitaillementEau);
 		this.interventionController.updateVehiculeStatut(vehicule);
 	}
 	
-	public boolean estEfficace(FireIntensity intensite) {
+	
+	/**
+	 * evalue l'efficacite aleatoire du vehicule a attenuer un feu 
+	 * @param intensity
+	 * @return
+	 */
+	public boolean estEfficace(FireIntensity intensity) {
 		Random r = new Random();
-		if (intensite.equals(FireIntensity.VeryHigh) && r.nextInt(100) < 10) {
+		if (intensity.equals(FireIntensity.VeryHigh) && r.nextInt(100) < 10) {
 			return true;
 		}
-		else if (intensite.equals(FireIntensity.High) && r.nextInt(100) < 40) {
+		else if (intensity.equals(FireIntensity.High) && r.nextInt(100) < 40) {
 			return true;
 		}
-		else if (intensite.equals(FireIntensity.Medium) && r.nextInt(100) < 70) {
+		else if (intensity.equals(FireIntensity.Medium) && r.nextInt(100) < 70) {
 			return true;
 		}
-		else if (intensite.equals(FireIntensity.Low) && r.nextInt(100) < 98) {
+		else if (intensity.equals(FireIntensity.Low) && r.nextInt(100) < 98) {
 			return true;
 		}
 		else {
@@ -181,6 +194,11 @@ public class Simulator {
 		}
 	}
 	
+	/**
+	 * aggravation du feu de façon aleatoire
+	 * @param intensite
+	 * @return
+	 */
 	public boolean SAggrave(FireIntensity intensite) {
 		Random r = new Random();
 		if (intensite.equals(FireIntensity.VeryHigh) && r.nextInt(100) < 20) {
